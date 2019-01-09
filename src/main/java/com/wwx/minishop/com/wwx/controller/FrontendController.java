8d6bf3381b83f83b14451c5a33e0c037c16fafec
wuwenxi <1139541228@qq.com>
2019-01-09 16:23:30 +0800

    初始页面

---
+++ src/main/java/com/wwx/minishop/com/wwx/controller/FrontendController.java
@@ -0,0 +1,10 @@
+package com.wwx.minishop.com.wwx.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/frontend")
+public class FrontendController {
+    
+}
